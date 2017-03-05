4d9cd48c16d33df90b615663e6050ae215ca8196
sherif98 <sheco_12345@hotmail.com>
2017-03-05 18:43:07 +0200

    creating separate controller

--- src/main/java/com/edu/heroku/test/TestController.java
+++ src/main/java/com/edu/heroku/test/TestController.java
@@ -2,6 +2,7 @@ package com.edu.heroku.test;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,4 +17,10 @@ public class TestController {
     public String home2() {
         return "Hello World ! test path";
     }
+
+    @RequestMapping(value = "/dis")
+    public String home3(@RequestParam String name) {
+        return "your name is " + name;
+    }
+
 }
